3ac97a2f0c3f345faa25b7ce3d4be710a0e47fee
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-05-23 17:25:28 +0530

    Update cust_filters.py

--- FallenRobot/modules/cust_filters.py
+++ FallenRobot/modules/cust_filters.py
@@ -15,23 +15,23 @@ from telegram.ext import (
 )
 from telegram.utils.helpers import escape_markdown, mention_html
 
-from FallenRobot import DRAGONS, LOGGER, dispatcher
-from FallenRobot.modules.connection import connected
-from FallenRobot.modules.disable import DisableAbleCommandHandler
-from FallenRobot.modules.helper_funcs.alternate import send_message, typing_action
-from FallenRobot.modules.helper_funcs.chat_status import user_admin
-from FallenRobot.modules.helper_funcs.extraction import extract_text
-from FallenRobot.modules.helper_funcs.filters import CustomFilters
-from FallenRobot.modules.helper_funcs.handlers import MessageHandlerChecker
-from FallenRobot.modules.helper_funcs.misc import build_keyboard_parser
-from FallenRobot.modules.helper_funcs.msg_types import get_filter_type
-from FallenRobot.modules.helper_funcs.string_handling import (
+from camillia import DRAGONS, LOGGER, dispatcher
+from camillia.modules.connection import connected
+from camillia.modules.disable import DisableAbleCommandHandler
+from camillia.modules.helper_funcs.alternate import send_message, typing_action
+from camillia.modules.helper_funcs.chat_status import user_admin
+from camillia.modules.helper_funcs.extraction import extract_text
+from camillia.modules.helper_funcs.filters import CustomFilters
+from camillia.modules.helper_funcs.handlers import MessageHandlerChecker
+from camillia.modules.helper_funcs.misc import build_keyboard_parser
+from camillia.modules.helper_funcs.msg_types import get_filter_type
+from camillia.modules.helper_funcs.string_handling import (
     button_markdown_parser,
     escape_invalid_curly_brackets,
     markdown_to_html,
     split_quotes,
 )
-from FallenRobot.modules.sql import cust_filters_sql as sql
+from camillia.modules.sql import cust_filters_sql as sql
 
 HANDLER_GROUP = 10
 
