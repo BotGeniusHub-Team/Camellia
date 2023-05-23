1f8f24a9f76f51faa403401fa3dc88216f91be92
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-05-23 17:24:49 +0530

    Update backups.py

--- FallenRobot/modules/backups.py
+++ FallenRobot/modules/backups.py
@@ -7,22 +7,22 @@ from telegram import ParseMode
 from telegram.error import BadRequest
 from telegram.ext import CommandHandler, run_async
 
-# from FallenRobot.modules.sql import warns_sql as warnssql
-import FallenRobot.modules.sql.blacklist_sql as blacklistsql
+# from camillia.modules.sql import warns_sql as warnssql
+import camillia.modules.sql.blacklist_sql as blacklistsql
 
-# from FallenRobot.modules.sql import cust_filters_sql as filtersql
-# import FallenRobot.modules.sql.welcome_sql as welcsql
-import FallenRobot.modules.sql.locks_sql as locksql
-import FallenRobot.modules.sql.notes_sql as sql
+# from camillia.modules.sql import cust_filters_sql as filtersql
+# import camillia.modules.sql.welcome_sql as welcsql
+import camillia.modules.sql.locks_sql as locksql
+import camillia.modules.sql.notes_sql as sql
 
-# from FallenRobot.modules.rules import get_rules
-import FallenRobot.modules.sql.rules_sql as rulessql
-from FallenRobot import EVENT_LOGS, LOGGER, OWNER_ID, SUPPORT_CHAT, dispatcher
-from FallenRobot.__main__ import DATA_IMPORT
-from FallenRobot.modules.connection import connected
-from FallenRobot.modules.helper_funcs.alternate import typing_action
-from FallenRobot.modules.helper_funcs.chat_status import user_admin
-from FallenRobot.modules.sql import disable_sql as disabledsql
+# from camillia.modules.rules import get_rules
+import camillia.modules.sql.rules_sql as rulessql
+from camillia import EVENT_LOGS, LOGGER, OWNER_ID, SUPPORT_CHAT, dispatcher
+from camillia.__main__ import DATA_IMPORT
+from camillia.modules.connection import connected
+from camillia.modules.helper_funcs.alternate import typing_action
+from camillia.modules.helper_funcs.chat_status import user_admin
+from camillia.modules.sql import disable_sql as disabledsql
 
 
 @run_async
@@ -325,7 +325,7 @@ def export_data(update, context):
         },
     }
     baccinfo = json.dumps(backup, indent=4)
-    with open("FallenRobot{}.backup".format(chat_id), "w") as f:
+    with open("camillia{}.backup".format(chat_id), "w") as f:
         f.write(str(baccinfo))
     context.bot.sendChatAction(current_chat_id, "upload_document")
     tgl = time.strftime("%H:%M:%S - %d/%m/%Y", time.localtime(time.time()))
@@ -341,15 +341,15 @@ def export_data(update, context):
         pass
     context.bot.sendDocument(
         current_chat_id,
-        document=open("FallenRobot{}.backup".format(chat_id), "rb"),
-        caption="📤*Successfully Exported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n\nNote: This `FallenRobot-Backup` was specially made for notes 📚.".format(
+        document=open("camillia{}.backup".format(chat_id), "rb"),
+        caption="📤*Successfully Exported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n\nNote: This `camillia-Backup` was specially made for notes 📚.".format(
             chat.title, chat_id, tgl
         ),
         timeout=360,
         reply_to_message_id=msg.message_id,
         parse_mode=ParseMode.MARKDOWN,
     )
-    os.remove("FallenRobot{}.backup".format(chat_id))  # Cleaning file
+    os.remove("camillia{}.backup".format(chat_id))  # Cleaning file
 
 
 # Temporary data
