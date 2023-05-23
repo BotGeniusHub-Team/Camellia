85ef5817e6581ba34800a9c5f7590e98b1d51563
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-05-23 17:30:22 +0530

    Update mongo.py

--- FallenRobot/utils/mongo.py
+++ FallenRobot/utils/mongo.py
@@ -1,9 +1,9 @@
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 
-from FallenRobot import MONGO_DB_URI
+from camillia import MONGO_DB_URI
 
 mongo = MongoCli(MONGO_DB_URI)
-db = mongo.FallenRobot
+db = mongo.camillia
 
 coupledb = db.couple
 
