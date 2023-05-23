1f213d6146fb325f552b1943fbbc91392d2cb518
Ishikki Akabane <rohansjaishwal@gmail.com>
2023-05-23 17:26:00 +0530

    Update fonts.py

--- FallenRobot/modules/fonts.py
+++ FallenRobot/modules/fonts.py
@@ -1,8 +1,8 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from FallenRobot import pbot as Client
-from FallenRobot.utils.fonts import Fonts
+from camillia import pbot as Client
+from camillia.utils.fonts import Fonts
 
 
 @Client.on_message(filters.command(["font", "fonts"]))
